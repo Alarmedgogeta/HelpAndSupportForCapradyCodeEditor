394e06993ea5f342fd892604e2d9039e68fb5d65
Alarmedgogeta <Alandiazyanez@outlook.com>
2021-01-11 23:24:22 -0600

    se trata de corregir bug en el ruteo del home en el servidor de github

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,22 +15,22 @@ const Home = () => (
     <Title level={2}>Documentacion de Caprady Code</Title>
     <Card title='Modulos'>
       <Grid style={gridStyle}>
-        <Link to='GetStarted'>
+        <Link to='/GetStarted'>
           <Card title='Para comenzar'>Mira una introduccion al lenguaje</Card>
         </Link>
       </Grid>
       <Grid style={gridStyle}>
-        <Link to='Tutorials'>
+        <Link to='/Tutorials'>
           <Card title='Tutoriales'>Ejemplos de código de programas sencillos</Card>
         </Link>
       </Grid>
       <Grid style={gridStyle}>
-        <Link to='LanguajesReferences'>
+        <Link to='/LanguajesReferences'>
           <Card title='Referencias de otros lenguajes'>Mira las inspiraciones del lenguaje</Card>
         </Link>
       </Grid>
       <Grid style={gridStyle}>
-        <Link to='About'>
+        <Link to='/About'>
           <Card title='Acerca de'>Aprende sobre el creador y el propósito del lenguaje</Card>
         </Link>
       </Grid>
